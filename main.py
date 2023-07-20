83e567907ec4d5aa971bc4f4e476bcd07864cbf5
Quality <qualityru@gmail.com>
2023-07-20 16:05:27 +0300

    Переход на ChimeraGPT

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-import io
 from os import getenv
 
 import openai
@@ -7,18 +6,18 @@ from aiogram import Bot, Dispatcher, F, Router
 from aiogram.enums.chat_action import ChatAction
 from aiogram.filters import Command
 from aiogram.methods import SendChatAction
-from aiogram.types import Message, Voice
+from aiogram.types import Message
 from dotenv import load_dotenv
-from pydub import AudioSegment
 
 load_dotenv()
 TELEGRAM_BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN')
 WHITE_LIST_IDS = getenv('WHITE_LIST_IDS').split(', ')
-OPENAI_API_KEY = getenv('OPENAI_API_KEY')
+CHIMERA_API_KEY = getenv('CHIMERA_API_KEY')
 GPT_ENGINE = getenv('GPT_ENGINE')
 
 
-openai.api_key = OPENAI_API_KEY
+openai.api_key = CHIMERA_API_KEY
+openai.api_base = 'https://chimeragpt.adventblocks.cc/v1'
 router: Router = Router()
 
 
@@ -26,25 +25,6 @@ async def create_chat_completion(message):
     return await openai.ChatCompletion.acreate(
         model=GPT_ENGINE, messages=[{'role': 'user', 'content': message}]
     )
-
-
-async def audio_to_text(file_path: str) -> str:
-    """Принимает путь к аудио файлу, возвращает текст файла."""
-    with open(file_path, 'rb') as audio_file:
-        transcript = await openai.Audio.atranscribe('whisper-1', audio_file)
-    return transcript['text']
-
-
-async def save_voice_as_mp3(bot: Bot, voice: Voice) -> str:
-    """Скачивает голосовое сообщение и сохраняет в формате mp3."""
-    voice_file_info = await bot.get_file(voice.file_id)
-    voice_ogg = io.BytesIO()
-    await bot.download_file(voice_file_info.file_path, voice_ogg)
-    voice_mp3_path = f'voice_files/voice-{voice.file_unique_id}.mp3'
-    AudioSegment.from_file(voice_ogg, format='ogg').export(
-        voice_mp3_path, format='mp3'
-    )
-    return voice_mp3_path
 
 
 @router.message(Command(commands=['start']))
@@ -81,34 +61,6 @@ async def process_text_message(message: Message):
     except Exception as e:
         msg = f'Ошибка: {e}'
     await message.answer(text=msg)
-
-
-@router.message(F.content_type == 'voice')
-async def process_voice_message(message: Message, bot: Bot):
-    """Принимает все голосовые сообщения и транскрибирует их в текст."""
-    if str(message.from_user.id) not in WHITE_LIST_IDS:
-        return
-    await SendChatAction(
-        chat_id=message.from_user.id, action=ChatAction.TYPING
-    )
-    voice_path = await save_voice_as_mp3(bot, message.voice)
-    transcripted_voice_text = await audio_to_text(voice_path)
-
-    if transcripted_voice_text:
-        await message.reply(text=transcripted_voice_text)
-        # await SendChatAction(
-        #     chat_id=message.from_user.id, action=ChatAction.TYPING
-        # )
-        # try:
-        #     msg = await create_chat_completion(transcripted_voice_text)
-        #     msg = msg.choices[0].message.content
-        # except openai.error.RateLimitError:
-        #     msg = ("Достигнут предел скорости. "
-        #            "Ограничение: 3 запроса в минуту. "
-        #            "Пожалуйста, повторите попытку через 20 секунд.")
-        # except Exception as e:
-        #     msg = f"Ошибка: {e}"
-        # await message.answer(text=msg)
 
 
 async def main():
